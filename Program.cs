d28584066181f70ce852763cc48bf683068c3e13
farhanmuh <97308802+farhanmuh@users.noreply.github.com>
2022-04-19 16:30:27 +0700

    Tugas Responsi

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan( 190302123, "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "Jono", 2000000);
+            
+
+            Console.WriteLine("No Nik\t    Nama\t  GajiBulanan");
+            Console.WriteLine("-------------------------------------------");
+            karyawan.karyawan();
+            karyawan2.karyawan();
+            
+
+            Console.WriteLine("\n\nAseekkkk Kenaikan Gaji 10%!!!\n\n");
+
+            Console.WriteLine("No Nik\t    Nama\t  GajiBulanan");
+            Console.WriteLine("-------------------------------------------");
+            karyawan.NaikGaji();
+            karyawan2.NaikGaji();
+            
+            Console.ReadKey();
+        }
+    }
+}
